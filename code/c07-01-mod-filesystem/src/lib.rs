0ff0f30599be7a88eb79b60318293fbb37d83b0a
Kenneth Powers <ken@kenpowers.net>
2018-08-31 22:00:18 -0400

    Add `use` and `super` to c07-01-mod-filesystem

--- code/c07-01-mod-filesystem/src/lib.rs
+++ code/c07-01-mod-filesystem/src/lib.rs
@@ -3,8 +3,9 @@ pub mod network; // network.rs
 
 #[cfg(test)]
 mod tests {
+    use super::client;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        client::connect();
     }
 }
