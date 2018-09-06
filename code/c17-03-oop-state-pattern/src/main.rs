3a1790acc74eb8e42d79691f1c4393a73e53dc78
Kenneth Powers <ken@kenpowers.net>
2018-09-06 09:11:54 -0400

    Add c17-03-oop-state-pattern

---
+++ code/c17-03-oop-state-pattern/src/main.rs
@@ -0,0 +1,18 @@
+extern crate blog;
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+    println!("Edited!");
+
+    post.request_review();
+    assert_eq!("", post.content());
+    println!("Review requested!");
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+    println!("Approved!");
+}
