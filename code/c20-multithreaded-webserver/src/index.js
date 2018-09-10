b2329b9467432b5e9bf59061bb34ef232d26524f
Kenneth Powers <ken@kenpowers.net>
2018-09-09 20:55:12 -0400

    Update c20-multithreaded-webserver
    Update servers with sleep route.

--- code/c20-multithreaded-webserver/src/index.js
+++ code/c20-multithreaded-webserver/src/index.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
     fs.createReadStream('index.html').pipe(res);
+  } else if (req.method === 'GET' && req.url === '/sleep') {
+      setTimeout(() => {
+        fs.createReadStream('index.html').pipe(res);
+      }, 5000);
   } else {
     res.writeHead(404);
     fs.createReadStream('404.html').pipe(res);
--- code/c20-multithreaded-webserver/src/main.rs
+++ code/c20-multithreaded-webserver/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::prelude::*;
 use std::net::{ TcpListener, TcpStream };
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -17,18 +19,22 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
     } else {
-        ("HTTP/1.1 404 Not Found\r\n\r\n", "404.html")
+        ("HTTP/1.1 404 Not Found", "404.html")
     };
 
     let mut contents = String::new();
     let mut file = File::open(filename).unwrap();
     file.read_to_string(&mut contents).unwrap();
 
-    let res = format!("{}{}", status_line, contents);
+    let res = format!("{}\r\n\r\n{}", status_line, contents);
 
     stream.write(res.as_bytes()).unwrap();
     // flush will wait and prevent the program from continuing until all the bytes are written to
